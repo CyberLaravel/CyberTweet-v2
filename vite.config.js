57f5950b9a2b4e4a63237ea12f4562f6e0d9eb4e
Hasnx <196798036+hasnx@users.noreply.github.com>
2025-02-24 16:13:26 +0100

    Simplify Vite configuration and remove unnecessary Vue alias

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
+            input: "resources/js/app.js",
+            ssr: "resources/js/ssr.js",
             refresh: true,
         }),
         vue({
@@ -19,10 +19,4 @@ export default defineConfig({
             },
         }),
     ],
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.esm-bundler.js',
-            '@': path.resolve(__dirname, './resources/js'),
-        },
-    },
 });
